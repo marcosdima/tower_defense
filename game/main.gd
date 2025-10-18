6b55bffc766ef89f4b9650567df862f624274c3d
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-18 12:30:12 -0300

    Rework logic

--- game/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@export var enemy_scene: PackedScene
-
-func spawn_enemy():
-	var enemy = enemy_scene.instantiate()
-	self.add_child(enemy)
-	$Path2D.start_walk(enemy)
--- game/scripts/path_controller.gd
+++
@@ -1,9 +0,0 @@
-class_name PathController
-extends Path2D
-
-func start_walk(walker: Walker):
-	walker.set_path(self)
-
-
-func ended(distance: float) -> bool:
-	return distance >= curve.get_baked_length()
