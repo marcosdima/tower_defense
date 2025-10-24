ef92c9ac64c00cd50d2db2bf366db59792d96e77
marcosdima <marcosdimatteo.wgr@gmail.com>
2025-10-24 11:31:22 -0300

    some main changes

--- main.gd
+++ main.gd
@@ -1,4 +1,13 @@
-extends Node2D
+extends Control
+
+@onready var menu: Control = $MainMenu
+@onready var level: Node2D = $Level
 
 func _ready() -> void:
 	pass
+
+
+func _on_main_menu_lets_play() -> void:
+	menu.visible = false
+	level.visible = true
+	level.start_level()
